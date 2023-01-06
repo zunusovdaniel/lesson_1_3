f881a30fc4455c5922650e551d2820f0143ead6e
zunusovdaniel <danielzunusov7@gmail.com>
2023-01-06 16:00:56 +0600

    исправил все

--- app/src/main/java/com/example/lesson_1_3/MainActivity.java
+++ app/src/main/java/com/example/lesson_1_3/MainActivity.java
@@ -13,33 +13,41 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button bute;
-    EditText edemail,edmassage,edtext;
-
+    EditText edemail, edmassage, edtext;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      bute = findViewById(R.id.button);
-      edemail =findViewById(R.id.ed_email);
-      edmassage=findViewById(R.id.ed_massage);
-      edtext=findViewById(R.id.ed_text);
-      bute.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              if (edemail.getText().toString().isEmpty()||edmassage.getText().toString().isEmpty() ||edtext.getText().toString().isEmpty()){
-                  Toast.makeText(getApplicationContext(),"заполнить все тексты",Toast.LENGTH_SHORT).show();
-              }else{
-                  Intent email_intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",edemail.getText().toString(), null));
-                  email_intent.putExtra(android.content.Intent.EXTRA_SUBJECT, edmassage.getText().toString());
-                  email_intent.putExtra(android.content.Intent.EXTRA_TEXT,edtext.getText().toString());
-
-                  startActivity(Intent.createChooser(email_intent, "Send email..."));
-              }
-          }
-      });
+        initView();
+        onClick();
     }
 
+    private void initView() {
+        bute = findViewById(R.id.button);
+        edemail = findViewById(R.id.ed_email);
+        edmassage = findViewById(R.id.ed_massage);
+        edtext = findViewById(R.id.ed_text);
+    }
 
+    private void onClick() {
+        bute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edemail.getText().toString().isEmpty() || edmassage.getText().toString().isEmpty() || edtext.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "заполнить все тексты", Toast.LENGTH_SHORT).show();
+                } else {
+                    initIntent();
+                }
+            }
+        });
+    }
+
+    private void initIntent() {
+        Intent email_intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", edemail.getText().toString(), null));
+        email_intent.putExtra(android.content.Intent.EXTRA_SUBJECT, edmassage.getText().toString());
+        email_intent.putExtra(android.content.Intent.EXTRA_TEXT, edtext.getText().toString());
+        startActivity(Intent.createChooser(email_intent, "Send email..."));
+    }
 }
--- app/src/main/java/com/example/lesson_1_3/secondActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.lesson_1_3;
-
-public class secondActivity {
-
-}
